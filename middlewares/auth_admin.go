7aa3836c168b8f336dd5e6df5bef1ec85fbd03cf
Noobly30 <jamesrobbins4024@gmail.com>
2023-10-17 20:54:00 +0800

    first commit

---
+++ middlewares/auth_admin.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"OJ-Test-System/helper"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthAdminCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.GetHeader("Authorization")
+		userClaim, err := helper.AnalyseToken(auth)
+		if err != nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized Authorization",
+			})
+			return
+		}
+		if userClaim == nil || userClaim.IsAdmin != 1 {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized Admin",
+			})
+			return
+		}
+		c.Next()
+	}
+}
---
+++ middlewares/auth_user.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"OJ-Test-System/helper"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthUserCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.GetHeader("Authorization")
+		userClaim, err := helper.AnalyseToken(auth)
+		if err != nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized Authorization",
+			})
+			return
+		}
+		if userClaim == nil {
+			c.Abort()
+			c.JSON(http.StatusOK, gin.H{
+				"code": http.StatusUnauthorized,
+				"msg":  "Unauthorized Admin",
+			})
+			return
+		}
+
+	}
+}
